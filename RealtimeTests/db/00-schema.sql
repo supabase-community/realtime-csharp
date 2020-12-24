3b0c6c9b11a4bd7286cee995ccaccb517097d145
Joseph Schultz <joseph@acupofjose.com>
2020-12-24 10:30:17 -0600

    Update postgres init schema to include configuration required by the `Realtime` package

--- RealtimeTests/db/00-schema.sql
+++ RealtimeTests/db/00-schema.sql
@@ -1,4 +1,6 @@
-﻿
+﻿ALTER SYSTEM SET wal_level = logical;
+ALTER SYSTEM SET max_replication_slots = 5;
+
 -- Create the Replication publication 
 CREATE PUBLICATION supabase_realtime FOR ALL TABLES;
 
