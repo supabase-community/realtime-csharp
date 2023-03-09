f0aec1361ab195a79f410a4a6e0793e3c0529fc9
Joseph Schultz <joseph@acupofjose.com>
2023-03-08 21:58:01 -0600

    Add connect/disconnect button to presence example

--- Examples/PresenceExample/Program.cs
+++ Examples/PresenceExample/Program.cs
@@ -1,11 +1,9 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Newtonsoft.Json;
 using PresenceExample;
 using Supabase.Realtime;
 using Supabase.Realtime.Socket;
-using System.Diagnostics;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var supabaseUrl = "https://ttbkuxsncbeeqnyeltrv.supabase.co";
 var supabasePublicKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMjIwMjE2MCwiZXhwIjoxOTI3Nzc4MTYwfQ.W03Slc1IFkth06FwutNVmorSKLLIjQ2f-bLJkNi51_Y";
@@ -16,6 +14,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddSingleton(builder => new Client(realtimeUrl, new ClientOptions
 {
