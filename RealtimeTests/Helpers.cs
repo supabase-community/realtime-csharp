94a706856e73f481375ef71415746b5c26280bea
Joseph Schultz <joseph@acupofjose.com>
2023-05-22 13:53:15 -0500

    Github actions are the bane of my existence.

--- RealtimeTests/Helpers.cs
+++ RealtimeTests/Helpers.cs
@@ -11,8 +11,8 @@ internal static class Helpers
     private const string ApiKey =
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE2NzEyMzc4NzMsImV4cCI6MjAwMjc3Mzk5MywiYXVkIjoiIiwic3ViIjoiIiwicm9sZSI6ImF1dGhlbnRpY2F0ZWQifQ.qoYdljDZ9rjfs1DKj5_OqMweNtj7yk20LZKlGNLpUO8";
 
-    private const string SocketEndpoint = "ws://realtime-dev.realtime-server:4000/socket";
-    private const string RestEndpoint = "http://rest:3000";
+    private const string SocketEndpoint = "ws://realtime-dev.localhost:4000/socket";
+    private const string RestEndpoint = "http://localhost:3000";
 
     public static Postgrest.Client RestClient() => new(RestEndpoint, new Postgrest.ClientOptions());
 
