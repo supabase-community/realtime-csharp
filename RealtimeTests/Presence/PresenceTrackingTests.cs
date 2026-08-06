c37834eba4e84f0384e5368bc0204c5b3257d08d
tr00d <guillaume.faas@live.fr>
2026-08-06 16:41:41 +0200

    refactor: test-suite rework

--- RealtimeTests/Presence/PresenceTrackingTests.cs
+++ RealtimeTests/Presence/PresenceTrackingTests.cs
@@ -1,50 +1,44 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
+using RealtimeTests.Models;
 using Supabase.Postgrest.Interfaces;
 using Supabase.Realtime;
 using Supabase.Realtime.Interfaces;
-using Supabase.Realtime.Models;
 
-namespace RealtimeTests;
+namespace RealtimeTests.Presence;
 
-public class PresenceExample : BasePresence
-{
-    [JsonProperty("time")] public DateTime? Time { get; set; }
-}
-
+/// <summary>
+///     Presence against the live stack: two clients tracking presence on the same channel each observe the
+///     other's state through the sync event.
+/// </summary>
 [TestClass]
-public class ChannelPresenceTests
+[TestCategory("E2E")]
+public class PresenceTrackingTests
 {
-    private IPostgrestClient? _restClient;
-    private IRealtimeClient<RealtimeSocket, RealtimeChannel>? _socketClient;
+    private IPostgrestClient restClient = null!;
+    private IRealtimeClient<RealtimeSocket, RealtimeChannel> socketClient = null!;
 
     [TestInitialize]
     public async Task InitializeTest()
     {
-        _restClient = Helpers.RestClient();
-        _socketClient = Helpers.SocketClient();
-        await _socketClient!.ConnectAsync();
+        restClient = Helpers.RestClient();
+        socketClient = Helpers.SocketClient();
+        await socketClient.ConnectAsync();
     }
 
     [TestCleanup]
-    public void CleanupTest()
-    {
-        _socketClient!.Disconnect();
-    }
+    public void CleanupTest() => socketClient.Disconnect();
 
-    [TestMethod("Channel: Can create presence")]
-    public async Task ClientCanCreatePresence()
+    [TestMethod]
+    public async Task Track_ShouldSyncPresenceBetweenClients()
     {
         var tsc = new TaskCompletionSource<bool>();
         var tsc2 = new TaskCompletionSource<bool>();
-
         var guid1 = Guid.NewGuid().ToString();
         var guid2 = Guid.NewGuid().ToString();
-
-        var channel1 = _socketClient!.Channel("online-users");
+        var channel1 = socketClient.Channel("online-users");
         var presence1 = channel1.Register<PresenceExample>(guid1);
         presence1.AddPresenceEventHandler(IRealtimePresence.EventType.Sync, (_, _) =>
         {
@@ -52,7 +46,6 @@ public class ChannelPresenceTests
             if (state.ContainsKey(guid2) && state[guid2].First().Time != null)
                 tsc.TrySetResult(true);
         });
-
         var client2 = Helpers.SocketClient();
         await client2.ConnectAsync();
         var channel2 = client2.Channel("online-users");
@@ -63,15 +56,11 @@ public class ChannelPresenceTests
             if (state.ContainsKey(guid1) && state[guid1].First().Time != null)
                 tsc2.TrySetResult(true);
         });
-
         await channel1.Subscribe();
         await channel2.Subscribe();
-
         await presence1.Track(new PresenceExample { Time = DateTime.Now });
         await presence2.Track(new PresenceExample { Time = DateTime.Now });
-
         await presence1.Untrack();
-        
-        await Task.WhenAll(new[] { tsc.Task, tsc2.Task });
+        await Task.WhenAll(tsc.Task, tsc2.Task);
     }
 }
